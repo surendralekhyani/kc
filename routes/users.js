c71f60f5a17351e4c9f3d7cc56b906e652db3848
Suri <surendralekhyani@gmail.com>
2023-10-24 18:27:47 +0530

    after exp-validation

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
 const dbConn = require("../lib/db");
 const bcrypt = require("bcrypt");
@@ -31,217 +32,112 @@ router.get("/add", function (req, res, next) {
   });
 });
 
-function checkEmail(email) {
+let validateInputs = [
+  check("name", "Name can not be blank...").trim().notEmpty().escape(),
+
+  check("email", "Invalid email").trim().isEmail(),
+
+  check("password", "Invalid password. Password must be at least 5 chars long")
+    .isLength({ min: 5 }),
+
+  check("confirmPassword", "Password confirmation does not match password")
+    .custom((value, { req }) => {
+      return value === req.body.password
+    })
+];
+
+
+let checkExistEmail = (email) => {
   return new Promise((resolve, reject) => {
-    const sql = "SELECT email FROM users WHERE email = ?";
-    dbConn.query(sql, [email], (err, results) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(results.length > 0);
-      }
-    });
+    try {
+      dbConn.query(
+        ' SELECT * FROM `users` WHERE `email` = ?  ', email,
+        function (err, rows) {
+          if (err) {
+            reject(err)
+          }
+          if (rows.length > 0) {
+            resolve(true)
+          } else {
+            resolve(false)
+          }
+        }
+      );
+    } catch (err) {
+      reject(err);
+    }
   });
-}
+};
+
+let createNewUser = (data) => {
+  return new Promise(async (resolve, reject) => {
+    // check email is exist or not
+    let isEmailExist = await checkExistEmail(data.email);
+    if (isEmailExist) {
+      reject(`This email "${data.email}" has already exist. Please choose an other email`);
+    } else {
+      // hash password
+      let salt = bcrypt.genSaltSync(10);
+      let userItem = {
+        fullname: data.fullname,
+        email: data.email,
+        password: bcrypt.hashSync(data.password, salt),
+      };
+
+      //create a new account
+      dbConn.query(
+        ' INSERT INTO users set ? ', userItem,
+        function (err, rows) {
+          if (err) {
+            reject(false)
+          }
+          resolve("Create a new user successful");
+        }
+      );
+    }
+  });
+};
+
 
 // add a new user
-router.post("/add", function (req, res, next) {
-  let name = req.body.name;
-  let email = req.body.email;
-  let password = req.body.password;
-  let confirmPassword = req.body.confirmPassword;
-  let errors = false;
-  let emailRegex =
-    /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
-
-  checkEmail(email).then((exists) => {
-    if (exists) {
-      // Email exist
-      errors = true;
-      req.flash("error", "EMAIL already exists...");
-      req.flash("error", "....!!!");
-      res.render("users/add", {
-        name: name,
-        email: email,
-        password: password,
-        confirmPassword: confirmPassword,
-      });
-    } // Email does not exist
-    else {
-      if (
-        name.length === 0 ||
-        email.length === 0 ||
-        password === 0 ||
-        name.length > 100 ||
-        email.length > 100
-      ) {
-        errors = true;
-        req.flash(
-          "error",
-          "Please enter name and email and password OR limit acceeded."
-        );
-      } else if (!emailRegex.test(email)) {
-        errors = true;
-        req.flash("error", "Invalid Email Address.");
-      } else if (password.length < 5 || password.length > 20) {
-        errors = true;
-        req.flash("error", "Required Min. Password length is 5");
-      } else if (password != confirmPassword) {
-        errors = true;
-        req.flash("error", "Password and Confirm Password r not same.");
-      }
-
-      // render to add.ejs with flash message
-      if (errors) {
-        req.flash("error", "....!!!");
-        res.render("users/add", {
-          name: name,
-          email: email,
-          password: password,
-          confirmPassword: confirmPassword,
-        });
-      }
-
-      // if no error
-      if (!errors) {
-        const saltRounds = 10;
-        bcrypt
-          .hash(password, saltRounds)
-          .then((hash) => {
-            var form_data = {
-              fullname: name,
-              email: email,
-              password: hash,
-            };
-
-            // insert query
-            dbConn.query(
-              "INSERT INTO users SET ?",
-              form_data,
-              function (err, result) {
-                if (err) {
-                  req.flash("error", err);
-                  res.render("users/add", {
-                    name: form_data.name,
-                    email: form_data.email,
-                    password: form_data.password,
-                  });
-                } else {
-                  req.flash("success", "User successfully added");
-                  res.redirect("/");
-                }
-              }
-            ); // query finished
-          }) // then close
-          .catch((err) => console.error(err.message));
-      } // End if(!errors)
-    }
-  });
-
-  console.log("End of this post");
-}); //post end
-
-// display edit user page
-router.get("/edit/(:id)", function (req, res, next) {
-  let id = req.params.id;
-
-  dbConn.query(
-    "SELECT * FROM users WHERE id = " + id,
-    function (err, rows, fields) {
-      if (err) throw err;
-
-      // if user not found
-      if (rows.length <= 0) {
-        req.flash("error", "User not found with id = " + id);
-        res.redirect("/users");
-      }
-      // if user found
-      else {
-        // render to edit.ejs
-        res.render("users/edit", {
-          title: "Edit User",
-          id: rows[0].id,
-          name: rows[0].name,
-          email: rows[0].email,
-          position: rows[0].position,
-        });
-      }
-    }
-  );
-});
-
-// update user data
-router.post("/update/:id", function (req, res, next) {
-  let id = req.params.id;
-  let name = req.body.name;
-  let email = req.body.email;
-  let position = req.body.position;
-  let errors = false;
-
-  if (name.length === 0 || email.length === 0 || position.length === 0) {
-    errors = true;
-
-    // set flash message
-    req.flash("error", "Please enter name and email and position");
-    // render to add.ejs with flash message
-    res.render("users/edit", {
-      id: req.params.id,
-      name: name,
-      email: email,
-      position: position,
+router.post("/add", validateInputs, async function (req, res, next) {
+  //validate required fields
+  let errorsArr = [];
+  let validationErrors = validationResult(req);
+  if (!validationErrors.isEmpty()) {
+    let errors = Object.values(validationErrors.mapped());
+    errors.forEach((item) => {
+      errorsArr.push(item.msg);
+    });
+    req.flash("error", errorsArr);
+    return res.render("users/add", {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword,
     });
   }
 
-  // if no error
-  if (!errors) {
-    var form_data = {
-      name: name,
-      email: email,
-      position: position,
-    };
-    // update query
-    dbConn.query(
-      "UPDATE users SET ? WHERE id = " + id,
-      form_data,
-      function (err, result) {
-        //if(err) throw err
-        if (err) {
-          // set flash message
-          req.flash("error", err);
-          // render to edit.ejs
-          res.render("users/edit", {
-            id: req.params.id,
-            name: form_data.name,
-            email: form_data.email,
-            position: form_data.position,
-          });
-        } else {
-          req.flash("success", "User successfully updated");
-          res.redirect("/users");
-        }
-      }
-    );
+  //create a new user
+  let newUser = {
+    fullname: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  };
+  try {
+    await createNewUser(newUser);
+    req.flash("success", "User successfully added");
+    return res.redirect("/");
+  } catch (err) {
+    req.flash("error", err);
+    res.render("users/add", {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword,
+    });
   }
 });
 
-// delete user
-router.get("/delete/(:id)", function (req, res, next) {
-  let id = req.params.id;
-
-  dbConn.query("DELETE FROM users WHERE id = " + id, function (err, result) {
-    //if(err) throw err
-    if (err) {
-      // set flash message
-      req.flash("error", err);
-      // redirect to user page
-      res.redirect("/users");
-    } else {
-      // set flash message
-      req.flash("success", "User successfully deleted! ID = " + id);
-      // redirect to user page
-      res.redirect("/users");
-    }
-  });
-});
 
 module.exports = router;
